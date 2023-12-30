d7d7d5b70b36846017dd6342f08c7060a1691df3
LucasAxelson <lucasaxelson2016@gmail.com>
2023-12-30 20:38:23 +0000

    Database connected - Task model, factory and migration completed

---
+++ laravel/database/factories/TaskFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence(1),
+            "description" => $this->faker->sentence(3),
+            "long_description" => $this->faker->paragraph(2),
+            "completed" => $this->faker->boolean(100),
+        ];
+    }
+}
